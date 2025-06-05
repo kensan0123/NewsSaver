9cf96e7db7710d7ff235979f9088ecdc7b70be39
Kensuke Nakamura <ken10232a@gmail.com>
2025-06-05 23:30:38 +0900

    警告除去、NewsURLHandlを実装

---
+++ NewsSaver/SaveNewsView/NewsURLHandle.swift
@@ -0,0 +1,18 @@
+//
+//  NewsURLHandle.swift
+//  NewsSaver
+//
+//  Created by 中村健介 on 2025/06/05.
+//
+
+import SwiftUI
+
+struct NewsURLHandle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsURLHandle()
+}
