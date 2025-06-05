0929fdb0b4698545d51f5a9ed9ae6b8eeca18ab7
Kensuke Nakamura <ken10232a@gmail.com>
2025-06-05 22:55:13 +0900

    NewsSaveView実装

---
+++ NewsSaver/SaveNewsView/NewsSaveView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsSaveView.swift
+//  NewsSaver
+//
+//  Created by 中村健介 on 2025/06/05.
+//
+
+import SwiftUI
+
+struct NewsSaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsSaveView()
+}
