00a148bf4629faa9aa4151015e451a8aa6b42441
Kensuke Nakamura <ken10232a@gmail.com>
2025-06-08 16:10:58 +0900

    ShareView機能を実装（不明なエラーあり）

---
+++ NewsSaver/ShareView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareView.swift
+//  NewsSaver
+//
+//  Created by 中村健介 on 2025/06/08.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareView()
+}
