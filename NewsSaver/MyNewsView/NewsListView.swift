0188bbbcef085b97e4af2f2e9dcadcb234e1af0f
Kensuke Nakamura <ken10232a@gmail.com>
2025-06-12 13:11:44 +0900

    削除機能の改善

--- NewsSaver/MyNewsView/NewsListView.swift
+++ NewsSaver/MyNewsView/NewsListView.swift
@@ -10,9 +10,14 @@ import SwiftData
 
 struct NewsListView: View {
     @Query var newsItems: [NewsItem]
+    @Environment(\.modelContext) private var modelContext
+    
     @State private var searchText: String = ""
     @State private var isSearching: Bool = false
     @State private var navigateToIntro = false
+    // 削除用
+    @State private var showDeleteAlert: Bool = false
+    @State private var itemsToDelete: [NewsItem] = []
     
     var filteredItems: [NewsItem] {
         if searchText.isEmpty {
@@ -37,34 +42,17 @@ struct NewsListView: View {
                 .padding(.top)
                 
                 if isSearching {
-                    TextField("Search", text: $searchText)
-                        .textFieldStyle(.roundedBorder)
-                        .padding(10)
-                        .overlay(
-                            HStack {
-                                Spacer()
-                                if !searchText.isEmpty {
-                                    Button(action: {
-                                        withAnimation{
-                                            searchText = ""
-                                            isSearching = false
-                                        }
-                                    }) {
-                                        Image(systemName: "xmark.circle.fill")
-                                            .foregroundStyle(.secondary)
-                                    }
-                                    .padding(.trailing, 16)
-                                }
-                            }
-                        )
-                        .padding(.horizontal)
-                        .padding(.vertical, 3)
-                        .background(Color.secondary.opacity(0.2))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    searchBox
                 }
-                List(filteredItems) { item in
-                    NavigationLink(destination: OpinionListView(news: item)) {
-                        NewsRowView(news: item)
+                List {
+                    ForEach(filteredItems, id: \.persistentModelID) { item in
+                        NavigationLink{OpinionListView(news: item)} label: {
+                            NewsRowView(news: item)
+                        }
+                    }
+                    .onDelete{ offsets in
+                        itemsToDelete = offsets.map {filteredItems[$0]}
+                        showDeleteAlert = true
                     }
                 }
                 .listStyle(.plain)
@@ -72,7 +60,50 @@ struct NewsListView: View {
                     IntroListView()
                 }
             }
+            .alert("このニュースを削除しますか？", isPresented: $showDeleteAlert) {
+                Button("削除", role: .destructive) {
+                        deleteSelectedItems()
+                }
+                Button("キャンセル", role: .cancel) {
+                    itemsToDelete.removeAll()
+                }
+            } message: {
+                Text("保存したニュースを削除しますか？")
+            }
         }
+    }
+    private var searchBox: some View {
+        TextField("Search", text: $searchText)
+            .textFieldStyle(.roundedBorder)
+            .padding(10)
+            .overlay(
+                HStack {
+                    Spacer()
+                    if !searchText.isEmpty {
+                        Button(action: {
+                            withAnimation{
+                                searchText = ""
+                                isSearching = false
+                            }
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.trailing, 16)
+                    }
+                }
+            )
+            .padding(.horizontal)
+            .padding(.vertical, 3)
+            .background(Color.secondary.opacity(0.2))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    private func deleteSelectedItems() {
+        for item in itemsToDelete {
+             modelContext.delete(item)
+        }
+        try? modelContext.save()
+        itemsToDelete.removeAll()
     }
 }
 
