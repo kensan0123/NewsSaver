d2936014dfcf071c750138c68b97eb6cab1eb2b9
Homayun Gohari <30705391+Homayuun@users.noreply.github.com>
2025-08-25 09:53:45 +0330

    Refactor: Move URL extraction logic to ShareViewController

--- NewsSaver/MyNewsView/NewsCategoryViews/Category1View.swift
+++ NewsSaver/MyNewsView/NewsCategoryViews/Category1View.swift
@@ -5,14 +5,14 @@
 //  Created by 中村健介 on 2025/07/08.
 //
 
-import SwiftUI
-
-struct Category1View: View {
-    var body: some View {
-        
-    }
-}
-
-#Preview {
-    Category1View()
-}
+//import SwiftUI
+//
+//struct Category1View: View {
+//    var body: some View {
+//        
+//    }
+//}
+//
+//#Preview {
+//    Category1View()
+//}
